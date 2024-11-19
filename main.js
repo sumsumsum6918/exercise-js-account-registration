ad18294e83896197f81eabdbaf19948c9969486f
sum <tinafongfong@gmail.com>
2024-11-19 20:39:01 +0100

    refactor and clean up

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const clickElement = (labelName) => {
   const inputElement = document.getElementById(`${labelName}-input`);
   const labelElement = document.getElementById(`${labelName}-label`);
 
+  inputElement.focus();
   if (!labelElement.classList.contains("show")) {
-    inputElement.focus();
     labelElement.classList.add("show");
-  } else return;
+  }
 };
 
 const form = document.querySelector("form");
@@ -16,40 +16,17 @@ const passwordInputElement = document.getElementById("password-input");
 const confirmInputElement = document.getElementById("confirm-password-input");
 const submitButton = document.querySelector(".sign-up-button");
 
-let pwValidation = false;
-
 form.addEventListener("focusout", (event) => {
-  if (event.target.type === "submit" || event.target.type === "button") return;
+  if (["submit", "button"].includes(event.target.type)) return;
   const id = event.target.id;
   const targetElement = id.substring(0, id.lastIndexOf("-input"));
   const warningElement = document.getElementById(`${targetElement}-warning`);
 
-  if (event.target.value && warningElement.classList.contains("show")) {
+  if (event.target.value) {
     warningElement.classList.remove("show");
   } else if (!event.target.value) {
     warningElement.classList.add("show");
   }
-  toggleSubmitEnabled();
-});
-
-function toggleRed(element) {
-  element.classList.add("red");
-  element.classList.remove("green");
-}
-function toggleGreen(element) {
-  element.classList.add("green");
-  element.classList.remove("red");
-}
-
-passwordInputElement.addEventListener("input", () => {
-  validatePassword();
-
-  toggleSubmitEnabled();
-});
-
-confirmInputElement.addEventListener("input", () => {
-  checkPassword();
-  toggleSubmitEnabled();
 });
 
 form.addEventListener("submit", (event) => {
@@ -63,33 +40,53 @@ form.addEventListener("submit", (event) => {
   console.log(registrationData);
 });
 
+function toggleRed(element) {
+  element.classList.add("red");
+  element.classList.remove("green");
+}
+function toggleGreen(element) {
+  element.classList.add("green");
+  element.classList.remove("red");
+}
+
 function validatePassword() {
   const length = passwordInputElement.value.length;
   if (length < 8 && length > 0) {
     toggleRed(passwordInputElement);
+    return "password";
   } else if (length >= 8) {
     toggleGreen(passwordInputElement);
-    if (confirmInputElement.value) {
-      checkPassword();
-    }
   }
 }
 
-function toggleSubmitEnabled() {
-  if (pwValidation) {
+function validatePasswordConfirmation() {
+  if (!confirmInputElement.value) return "confirmation";
+  if (confirmInputElement.value !== passwordInputElement.value) {
+    toggleRed(confirmInputElement);
+    return "confirmation";
+  } else if (confirmInputElement.value === passwordInputElement.value) {
+    toggleGreen(confirmInputElement);
+  }
+}
+
+function toggleSubmitEnabled(shouldEnable) {
+  if (shouldEnable) {
     submitButton.classList.add("submit");
     submitButton.type = "submit";
-  } else if (!pwValidation) {
+  } else if (!shouldEnable) {
     submitButton.classList.remove("submit");
     submitButton.type = "button";
   }
 }
-function checkPassword() {
-  if (confirmInputElement.value !== passwordInputElement.value) {
-    toggleRed(confirmInputElement);
-    pwValidation = false;
-  } else if (confirmInputElement.value === passwordInputElement.value) {
-    toggleGreen(confirmInputElement);
-    pwValidation = true;
-  }
+
+function validateForm() {
+  const passwordValidationError = validatePassword();
+  const confirmationValidationError = validatePasswordConfirmation();
+  const shouldEnableSubmit =
+    !passwordValidationError && !confirmationValidationError;
+  toggleSubmitEnabled(shouldEnableSubmit);
 }
+
+document.addEventListener("input", () => {
+  validateForm();
+});
